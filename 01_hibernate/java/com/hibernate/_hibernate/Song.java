dec1d0fa8d0adc0ff34a653710ae83352ebe3795
prakash kumar pradhan <111282629+Prakash11122@users.noreply.github.com>
2025-08-09 15:19:34 +0530

    save data using hibernate orm

--- 01_hibernate/java/com/hibernate/_hibernate/Song.java
+++ 01_hibernate/java/com/hibernate/_hibernate/Song.java
@@ -1,44 +1,29 @@
 package com.hibernate._hibernate;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
-@Entity
 public class Song {
 	
-	//songid, songName, singer
-
-	@Id
-	@Column(name = "songid")
 	private int id;
-	@Column(name = "songName")
 	private String songName;
-	@Column(name = "singer")
 	private String artist;
-
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getSongName() {
 		return songName;
 	}
-
 	public void setSongName(String songName) {
 		this.songName = songName;
 	}
-
 	public String getArtist() {
 		return artist;
 	}
-
 	public void setArtist(String artist) {
 		this.artist = artist;
 	}
+	
+	
 
 }
--- 01_hibernate/src/main/java/com/hibernate/_hibernate/App.java
+++ 01_hibernate/src/main/java/com/hibernate/_hibernate/App.java
@@ -4,31 +4,35 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class App 
-{
-    public static void main( String[] args )
-    {	
-    	
-    	//create configuration
-    	Configuration configuration = new Configuration();
-    	configuration.configure("hibernate.cfg.xml");
-    	
-    	//create session factory
-    	SessionFactory sessionFactory = configuration.buildSessionFactory();
-    	
-    	
-    	
-    	//initialize the session object
-    	Session session = sessionFactory.openSession();
-    	
-    	
-    	
-        Song song1 = new Song();
-        song1.setId(5);
-        song1.setSongName("saiyyan");
-        song1.setArtist("Kailash");
-        
-        session.save(song1);
-        System.out.println("song saved...");
-    }
+public class App {
+	public static void main(String[] args) {
+		
+
+		
+
+		// create configuration
+		Configuration configuration = new Configuration();
+		configuration.configure("hibernate.cfg.xml");
+		configuration.addAnnotatedClass(Song.class);
+
+		// create session factory
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+		// initialize the session object
+		Session session = sessionFactory.openSession();
+
+		Song song1 = new Song();
+
+		song1.setId(55);
+		song1.setSongName("Wafe ne bewafai");
+		song1.setArtist("Arijit shing");
+		
+		session.beginTransaction();
+		session.save(song1);
+		session.getTransaction().commit();
+
+		session.save(song1);
+
+		System.out.println("song saved...saiyyan");
+	}
 }
