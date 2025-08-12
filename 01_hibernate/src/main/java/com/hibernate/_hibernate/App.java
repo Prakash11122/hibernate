f5832a85cf48874e23a7c4762931c31e88b4a641
prakash kumar pradhan <111282629+Prakash11122@users.noreply.github.com>
2025-08-12 10:43:29 +0530

    meta

--- 01_hibernate/src/main/java/com/hibernate/_hibernate/App.java
+++ 01_hibernate/src/main/java/com/hibernate/_hibernate/App.java
@@ -7,12 +7,12 @@ import org.hibernate.cfg.Configuration;
 public class App {
 	public static void main(String[] args) {
 		
-
 		
 
 		// create configuration
 		Configuration configuration = new Configuration();
-		configuration.configure("hibernate.cfg.xml");
+		//configuration.configure("hibernate.cfg.xml");  if you remove this configuration file still it work fine,.
+		configuration.configure();
 		configuration.addAnnotatedClass(Song.class);
 
 		// create session factory
@@ -23,9 +23,9 @@ public class App {
 
 		Song song1 = new Song();
 
-		song1.setId(55);
-		song1.setSongName("Wafe ne bewafai");
-		song1.setArtist("Arijit shing");
+		song1.setId(66);
+		song1.setSongName("done ne bewafai");
+		song1.setArtist("neha shing");
 		
 		session.beginTransaction();
 		session.save(song1);
